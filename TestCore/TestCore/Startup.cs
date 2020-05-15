b4f97afe88bbaae397d0f67b04038822cc1e9ddc
Grant Smith <gsmith@tampabay.com>
2020-05-15 14:25:03 -0400

    DB created and init migration applied

--- TestCore/TestCore/Startup.cs
+++ TestCore/TestCore/Startup.cs
@@ -27,7 +27,7 @@ namespace TestCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite("ConectionString"));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
